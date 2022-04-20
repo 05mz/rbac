d7f020a88bbb6137e4d9601c8bb4e2804d6e3caf
Zaid Mohammad <74952827+05mz@users.noreply.github.com>
2022-04-20 17:23:21 +0530

    Update __init__.py

--- pythonProject1/__init__.py
+++ pythonProject1/__init__.py
@@ -13,35 +13,24 @@ RESOURCES = ['file']
 
 
 class RBAC:
-    """
-    Implements Role Based Access Control.
-    System should be able to assign a role to user and remove
-    a user from the role.
-    """
+   
 
     def __init__(self):
         #  Create a Access Control List
         self.acl = Registry()
 
     def add_role(self):
-        """Adds roles available in ACCESS_LEVELS.
-        """
-        # Assumption: admin user having access level(READ and WRITE)
-        # Syntax: acl.addRole(role, [access-levels])
+       
         for role, access_levels in ACCESS_LEVELS.items():
             self.acl.addRole(role, access_levels)
 
     def add_resource(self):
-        """Adds Resources available in RESOURCES.
-        """
-        # Assumption: 'file' is a resource
-        # Syntax: acl.addResource(resource-name)
+       
         for resource in RESOURCES:
             self.acl.addResource(resource)
 
     def allow_rule(self):
-        """Implements allow rules.
-        """
+      
         for role, access_levels in ACCESS_LEVELS.items():
             for acl in access_levels:
                 # Add allow Rules
@@ -49,8 +38,7 @@ class RBAC:
                 self.acl.allow(role, acl, RESOURCES)
 
     def deny_rule(self):
-        """Implements Deny rules.
-        """
+     
         for role, access_levels in ACCESS_LEVELS.items():
             # take all access levels from super-admin role
             for acl in ACCESS_LEVELS['super-admin']:
@@ -60,18 +48,13 @@ class RBAC:
                     self.acl.deny(role, acl, RESOURCES)
 
     def add_users(self):
-        """Add users with roles.
-        """
+       
         # Syntax: acl.addUser(user, [roles])
         self.acl.addUser('hrishi', ['non-admin', 'admin'])
         self.acl.addUser('user', ['super-admin'])
 
     def perform_operations(self):
-        """Use It to Check Permission
-        Given a user, action type and resource system should be able
-        to tell whether user has access or not.
-        """
-
+       
         # Before deleting role
         self.acl.isAllowed('hrishi', 'READ', 'file')
         self.acl.isAllowed('hrishi', 'WRITE', 'file')
