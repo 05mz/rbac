ba9213a0650e3e9d9fc40e3d5b5506415ad2bcf6
Zaid Mohammad <74952827+05mz@users.noreply.github.com>
2022-04-20 17:22:17 +0530

    Update acl.py

--- pythonProject1/acl.py
+++ pythonProject1/acl.py
@@ -6,22 +6,13 @@ from rbac.action_type import ActionType
 
 
 class Registry(ActionType):
-    """
-    The registry of access control list.
-    Implements code to check whether a particular user holds
-    the permission to access particular resource or not.
-    """
+   
 
     def __init__(self):
         super().__init__()
 
     def isAllowed(self, user, action_type, resource):
-        """Check the permission.
-        if the access is denied, this method will return False;
-        if the access is allowed, this method will return True;
-        if there is no any rule for the access, this method
-        will return None.
-        """
+       
 
         # check whether user and resource are present or not
         assert not user or user in self.users
@@ -30,11 +21,7 @@ class Registry(ActionType):
         # get roles of user
         roles = self.users[user]
 
-        # considering only super set of role
-        # e.x. if user having non-admin and admin role, then
-        # non-admin is subset of admin.
-        # in such case consider only admin role
-        # only one role for user
+       
         if len(roles) == 1:
             role = list(roles)[0]
         if len(roles) > 1:
@@ -43,9 +30,7 @@ class Registry(ActionType):
                 if role == 'super-admin' or role == 'admin':
                     break
 
-        # first check deny rule
-        # if role not present in denied it will give keyerror
-        # then check allowed rules
+   
         try:
             if self.denied[role, action_type.upper(), resource]:
                 print("Error: Permission denied. User: {0} has no {1} "
