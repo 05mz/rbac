6907340be7b6a32f1a6d23be0a79bac29822eed9
Zaid Mohammad <74952827+05mz@users.noreply.github.com>
2022-04-20 17:23:46 +0530

    Update action_type.py

--- pythonProject1/action_type.py
+++ pythonProject1/action_type.py
@@ -8,10 +8,7 @@ from rbac.resources import Resources
 
 
 class ActionType(Users, Roles, Resources):
-    """
-    Implements allow and deny functionalities to perform actions like
-    allow and deny based on access levels (READ, WRITE, DELETE).
-    """
+  
 
     def __init__(self):
         super().__init__()
@@ -21,8 +18,7 @@ class ActionType(Users, Roles, Resources):
         self.denied = {}
 
     def allow(self, role, action_type, resources=[]):
-        """Add a allowed rule for specific resources.
-        """
+       
         # validation of roles and action_types
         assert not role or role in self.roles
         assert not action_type or action_type in self.action_types
@@ -31,8 +27,7 @@ class ActionType(Users, Roles, Resources):
             self.allowed[role, action_type.upper(), resource] = True
 
     def deny(self, role, action_type, resources=[]):
-        """Add a denied rule for specific resources.
-        """
+      
         # validation of roles and action_types
         assert not role or role in self.roles
         assert not action_type or action_type in self.action_types
